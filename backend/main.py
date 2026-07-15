aac3bf96647fc274cbf49d11546b7d47a6dd80ce
Tanmaya <tanmayakharsel22@gmail.com>
2026-07-16 01:12:54 +0530

    Implement JWT authentication

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine, SessionLocal
 from models import Base, Note, User
-from schemas import NoteCreate, UserCreate
+from schemas import NoteCreate, UserCreate, UserLogin
+from security import hash_password, verify_password
+from jwt_handler import create_access_token
 
 Base.metadata.create_all(bind=engine)
 
@@ -82,7 +84,7 @@ def register(user: UserCreate):
     existing_user = db.query(User).filter(
         User.email == user.email
     ).first()
-    
+
     if existing_user:
         db.close()
         return {
@@ -90,9 +92,12 @@ def register(user: UserCreate):
         }
 
     new_user = User(
-        email=user.email,
-        password=user.password
-    )
+
+    email=user.email,
+
+    password=hash_password(user.password)
+
+)
 
     db.add(new_user)
     db.commit()
@@ -102,3 +107,41 @@ def register(user: UserCreate):
     return {
         "message": "User Registered Successfully"
     }
+
+@app.post("/login")
+def login(user: UserLogin):
+
+    db = SessionLocal()
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        db.close()
+        return {
+            "message": "User not found"
+        }
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        db.close()
+        return {
+            "message": "Incorrect Password"
+        }
+
+    token = create_access_token(
+    {
+        "user_id": db_user.id,
+        "email": db_user.email
+    }
+)
+
+    db.close()
+
+    return {
+    "access_token": token,
+    "token_type": "bearer"
+}
--- backend/schemas.py
+++ backend/schemas.py
@@ -2,14 +2,15 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-
     email: EmailStr
+    password: str
 
+
+class UserLogin(BaseModel):
+    email: EmailStr
     password: str
 
 
 class NoteCreate(BaseModel):
-
     title: str
-
     note: str
